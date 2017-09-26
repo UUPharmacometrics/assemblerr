442ed4e0c0e2fb37fdad5a4dd0bee8016b1e6685
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2017-09-26 07:13:13 +0200

    added test for eta code

--- tests/testthat/test-nm-variable-facets.R
+++ tests/testthat/test-nm-variable-facets.R
@@ -1,5 +1,4 @@
 context("NM variable facet")
-
 test_that("automatic index generation works", {
   theta_facet <- NMThetaFacet$new()
   theta <- theta_facet$add_new(name = "test")
@@ -10,14 +9,12 @@ test_that("automatic index generation works", {
 
 
 context("NM THETA facets")
-
 test_that("adding an unnamed theta results in an error", {
   theta_facet <- NMThetaFacet$new()
   expect_error(theta_facet$add_new())
 })
 
 context("NM THETA syntax element")
-
 test_that("code generation for THETA elements works", {
   theta <- NMTheta$new("Clearance")
   theta$index <- 1
@@ -25,4 +22,9 @@ test_that("code generation for THETA elements works", {
 })
 
 
-
+context("NM ETA syntax element")
+test_that("code generation for ETA elements works", {
+  eta <- NMEta$new("Clearance")
+  eta$index <- 1
+  expect_equal(eta$get_code(), "ETA(1)")
+})
