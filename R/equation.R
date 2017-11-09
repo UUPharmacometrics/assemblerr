4026e91e75022568ff1265a53a37aac04507e799
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2017-11-09 09:27:08 +0100

    added exort statements

--- R/equation.R
+++ R/equation.R
@@ -1,3 +1,4 @@
+#' @export
 equation <- function(expr){
   expr <- rlang::enexpr(expr)
   if(rlang::is_formula(expr)) {
@@ -11,9 +12,9 @@ equation <- function(expr){
       structure(class = "equation")
   }
 }
-
+#' @export
 as_equation <- function(x) UseMethod("as_equation")
-
+#' @export
 as_equation.character <- function(x){
   expr <- rlang::parse_expr(paste0("~", x))
   if(rlang::is_formula(expr)){
@@ -22,32 +23,35 @@ as_equation.character <- function(x){
     stop("Couldn't interpret text '", x , "' as an equation")
   }
 }
-
+#' @export
 empty_equation <- function() return(structure(list(), class = "equation"))
 
+is_equationish <- function(o) return(rlang::is_formulaish(o) | is(o, "equation"))
+
+#' @export
 as_equation.language <- function(x){
   list(lhs = rlang::f_lhs(x),
        rhs = rlang::f_rhs(x)
   ) %>% structure(class = "equation")
 }
-
+#' @export
 as_equation.formula <- function(x){
   list(lhs = rlang::f_lhs(x),
        rhs = rlang::f_rhs(x)
   ) %>% structure(class = "equation")
 }
-
+#' @export
 set_rhs <- function(eqn, rhs){
   purrr::update_list(eqn, rhs = rlang::enexpr(rhs))
 }
-
+#' @export
 set_lhs <- function(eqn, lhs){
   purrr::update_list(eqn, lhs = rlang::enexpr(lhs))
 
 }
-
+#' @export
 as_equation.equation <- function(x) x
-
+#' @export
 print.equation <- function(x){
   cat("Equation:\n")
   if(!is.null(x$lhs)){
@@ -56,7 +60,7 @@ print.equation <- function(x){
     cat("\t", deparse(x$rhs))
   }
 }
-
+#' @export
 `+.equation` <- function(x, y){
   stopifnot(is(y, "equation"))
   if(rlang::is_empty(x) && rlang::is_empty(y)) return(as_equation(~0))
@@ -65,7 +69,7 @@ print.equation <- function(x){
   x$rhs <- base::substitute(x + y, list(x = x$rhs, y = y$rhs))
   x
 }
-
+#' @export
 `-.equation` <- function(x, y){
   stopifnot(is(y, "equation"))
   if(rlang::is_empty(x)&&rlang::is_empty(y)) return(as_equation(~0))
@@ -79,18 +83,18 @@ print.equation <- function(x){
   }
 }
 
-
+#' @export
 variables <- function(x){
   if(!is(x, "equation")) stop("Function expects an equation as input")
   all.vars(x$rhs)
 }
 
 
-
+#' @export
 substitute_indicies <- function(eqn, array_name, substitutions){
   purrr::modify(eqn, ~transform_ast(.x, index_transformer, array_name = array_name, substitutions = substitutions))
 }
-
+#' @export
 index_transformer <- function(node, array_name, substitutions){
   # if vector access
   if(rlang::is_lang(node) && rlang::lang_name(node) == "[" && node[[2]] == rlang::sym(array_name)){
@@ -99,7 +103,7 @@ index_transformer <- function(node, array_name, substitutions){
   }
   node
 }
-
+#' @export
 transform_ast <- function(node, transformer, ...){
   if(rlang::is_atomic(node) || rlang::is_symbol(node)) return(transformer(node, ...))
   else if(rlang::is_lang(node)){
@@ -117,13 +121,13 @@ transform_ast <- function(node, transformer, ...){
   }
 }
 
-
+#' @export
 substitute <- function(x, ...) UseMethod("substitute")
-
+#' @export
 substitute.default <- base::substitute
-
+#' @export
 substitute.equation <- function(x, ...){
   args <- rlang::dots_list(...)
-  purrr::map_if(x, purrr::negate(is.null), ~pryr::substitute_q(.x, args)) %>%
+  purrr::map_if(x, is.language, ~pryr::substitute_q(.x, args)) %>%
     structure(class = "equation")
 }
