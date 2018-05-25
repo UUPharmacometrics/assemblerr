ce4da78c3b428edc00b874717e30b901f73b34d0
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2018-05-25 15:46:14 +0200

    added function to query for avaiable converters

--- R/aab_converters.R
+++ R/aab_converters.R
@@ -16,3 +16,18 @@ get_converter <- function(facet, name, target){
 call_converter <- function(facet, name, from, to, fragment) {
   get_converter(facet, name, class(to)[1])(to, from, fragment)
 }
+
+#' @export
+list_converters <- function(query = "", target = NULL){
+  matches <- converters %>%
+    purrr::map(dplyr::select_at, c("name", "target")) %>%
+    purrr::imap(~tibble::tibble(facet = .y, data = list(.x))) %>%
+    dplyr::bind_rows() %>%
+    tidyr::unnest() %>%
+    dplyr::filter(grepl(query, name)|grepl(query, facet))
+  if(!is.null(target)) matches <- matches %>%  dplyr::filter(target == !!(target))
+  return(matches)
+}
+
+#' @export
+lc <- list_converters
