a48b1b4fdb1aa08e301139ada8769db81e6a6d31
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2019-08-08 16:22:25 +0200

    fixed declaration conversion

--- R/algebraics.R
+++ R/algebraics.R
@@ -15,17 +15,16 @@ add_declarations_to_facets.nm_model <- function(to, from, dl){
   odes <- generate_ode_equations(from)
   ode_identifiers <- purrr::map_chr(odes, ~dec_get_id(.x) %>% deparse())
   dl %>%
-    purrr::reduce(.init = to, function(to, d){
+    purrr::map(function(d){
       # name of the variable being defined
       var <- dec_get_id(d) %>% deparse()
       if(!depends_on(var, "A", dl)){
-        return(to + nm_pk("", as_statement(dl)))
+        nm_pk("", as_statement(d))
+      }else if(any(purrr::map_lgl(ode_identifiers, ~depends_on(.x, var, odes)))){
+           nm_des("", as_statement(d))
       }else{
-        if(any(purrr::map_lgl(ode_identifiers, ~depends_on(.x, var, odes)))){
-          return(to + nm_des("", as_statement(d)))
-        }else{
-          return(to + nm_error("", as_statement(d)))
-        }
+          nm_error("", as_statement(d))
       }
-    })
+    }) %>%
+    purrr::reduce(.init = to, `+`)
 }
