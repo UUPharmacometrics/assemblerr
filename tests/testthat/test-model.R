48524be55be1539b89f2a18ac677421e00b32cbe
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2019-08-15 22:29:39 +0200

    added tests for model manipulation

--- tests/testthat/test-model.R
+++ tests/testthat/test-model.R
@@ -23,3 +23,22 @@ test_that("flows can be added", {
   expect_equal(dec_get_def(m$flows[["definition"]][[1]]), quote(k))
   expect_equal(m$flows[["index"]][1], 1)
 })
+
+
+test_that("algebraics can be added", {
+  m <- model() +
+    algebraic(effect~emax*conc/(conc+ec50))
+  expect_equal(dec_get_def(m$algebraics[["definition"]][[1]]), quote(emax*conc/(conc+ec50)))
+  expect_equal(dec_get_id(m$algebraics[["definition"]][[1]]), quote(effect))
+  expect_equal(m$algebraics[["name"]][1], "effect")
+  expect_equal(m$algebraics[["index"]][1], 1)
+})
+
+test_that("meta_tags can be added", {
+  m <- model() +
+    meta_tag("name", "test")
+  expect_equal(m$meta_tags[["value"]], "test")
+  expect_equal(m$meta_tags[["name"]], "name")
+  expect_equal(m$meta_tags[["index"]], 1)
+})
+
