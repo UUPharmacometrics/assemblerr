25562aa70a05c735fd5b57360e14d7d1abda752f
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2019-09-05 22:34:53 +0200

    switched to formulas

--- R/algebraics.R
+++ R/algebraics.R
@@ -1,29 +1,29 @@
 add_algebraics <- function(to, from) {
-  dl <- from$algebraics %>%
+  fmls <- from$algebraics %>%
     purrr::transpose() %>%
     purrr::map("definition") %>%
     purrr::map(replace_compartment_references, to = to, from = from) %>%
-    topologic_sort()
-  add_declarations_to_facets(to, from, dl)
+    fmls_topologic_sort()
+  add_declarations_to_facets(to, from, fmls)
 }
 
-# function to determine whether an algebraic declaration needs to go to PK, DES or ERROR
-add_declarations_to_facets <- function(to, from, dl) UseMethod("add_declarations_to_facets")
+# function to determine whether an algebraic formula needs to go to PK, DES or ERROR
+add_declarations_to_facets <- function(to, from, fmls) UseMethod("add_declarations_to_facets")
 
-add_declarations_to_facets.nm_model <- function(to, from, dl){
+add_declarations_to_facets.nm_model <- function(to, from, fmls){
   # get lhs of ODEs
   odes <- generate_ode_equations(from)
-  ode_identifiers <- purrr::map_chr(odes, ~dec_get_id(.x) %>% deparse())
-  dl %>%
-    purrr::map(function(d){
+  ode_identifiers <- purrr::map_chr(odes, ~fml_get_lhs(.x) %>% deparse())
+  fmls %>%
+    purrr::map(function(fml){
       # name of the variable being defined
-      var <- dec_get_id(d) %>% deparse()
-      if(!depends_on(var, "A", dl)){
-        nm_pk("", as_statement(d))
-      }else if(any(purrr::map_lgl(ode_identifiers, ~depends_on(.x, var, odes)))){
-           nm_des("", as_statement(d))
+      var <- fml_get_lhs(fml) %>% deparse()
+      if(!fml_depends_on(var, "A", fmls)){
+        nm_pk("", as_expr(fml))
+      }else if(any(purrr::map_lgl(ode_identifiers, ~fml_depends_on(.x, var, odes)))){
+           nm_des("", as_expr(fml))
       }else{
-          nm_error("", as_statement(d))
+          nm_error("", as_expr(fml))
       }
     }) %>%
     purrr::reduce(.init = to, `+`)
