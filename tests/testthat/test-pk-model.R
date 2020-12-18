4937d8bf5b21aa2e79e9c223615fdd3e7952651b
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2020-12-18 15:10:58 +0100

    add 1cmp, oral test

--- tests/testthat/test-pk-model.R
+++ tests/testthat/test-pk-model.R
@@ -11,8 +11,8 @@ test_that("1cmp linear, advan", {
   render(m) %>%
     expect_contains("VC = THETA(1) * EXP(ETA(1))") %>%
     expect_contains("CL = THETA(2) * EXP(ETA(2))") %>%
-    expect_contains("ADVAN1") %>%
-    expect_contains("TRANS2") %>%
+    expect_contains(" ADVAN1 ") %>%
+    expect_contains(" TRANS2 ") %>%
     expect_contains("CONC = A(1)/VC") %>%
     expect_contains("Y = CONC + EPS(1)")
 })
@@ -31,5 +31,24 @@ test_that("1cmp linear, ode", {
     expect_contains("CL = THETA(2) * EXP(ETA(2))") %>%
     expect_contains("DADT(1) = -(CL * (A(1)/VC))") %>%
     expect_contains("CONC = A(1)/VC") %>%
+    expect_contains("Y = CONC + EPS(1)")
+})
+
+
+test_that("1cmp linear 1st order absorption, advan", {
+  m <- pk_model() +
+    pk_absorption_rate_fo() +
+    pk_distribution_1cmp() +
+    pk_elimination_linear() +
+    obs_additive(conc~C["central"])
+
+  render(m) %>%
+    expect_contains(" ADVAN2 ") %>%
+    expect_contains(" TRANS2 ") %>%
+    expect_contains("MAT = THETA(1) * EXP(ETA(1))") %>%
+    expect_contains("VC = THETA(2) * EXP(ETA(2))") %>%
+    expect_contains("CL = THETA(3) * EXP(ETA(3))") %>%
+    expect_contains("KA = 1/MAT") %>%
+    expect_contains("CONC = A(2)/VC") %>%
     expect_contains("Y = CONC + EPS(1)")
 })
