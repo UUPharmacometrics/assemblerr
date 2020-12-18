2bb360ffd7df1641276a68e1bc02a65f702f7db0
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2020-12-18 16:10:04 +0100

    add 2cmp linear tests

--- tests/testthat/test-pk-model.R
+++ tests/testthat/test-pk-model.R
@@ -34,6 +34,39 @@ test_that("1cmp linear, ode", {
     expect_contains("Y = CONC + EPS(1)")
 })
 
+test_that("2cmp linear", {
+  m <- pk_model() +
+    pk_distribution_2cmp() +
+    pk_elimination_linear() +
+    obs_additive(conc~C["central"])
+
+  render(m,
+         options = assemblerr_options(ode.use_special_advans = FALSE,
+                                      ode.use_general_linear_advans = FALSE)
+  ) %>%
+    expect_contains("VC = THETA(1) * EXP(ETA(1))") %>%
+    expect_contains("VP = THETA(2) * EXP(ETA(2))") %>%
+    expect_contains("Q = THETA(3) * EXP(ETA(3))") %>%
+    expect_contains("CL = THETA(4) * EXP(ETA(4))") %>%
+    expect_contains("DADT(1) = Q * (A(2)/VP) - (Q * (A(1)/VC) + CL * (A(1)/VC))") %>%
+    expect_contains("DADT(2) = Q * (A(1)/VC) - Q * (A(2)/VP)") %>%
+    expect_contains("CONC = A(1)/VC") %>%
+    expect_contains("Y = CONC + EPS(1)")
+
+  render(m,
+         options = assemblerr_options(ode.use_special_advans = TRUE,
+                                      ode.use_general_linear_advans = FALSE)
+  ) %>%
+    expect_contains("VC = THETA(1) * EXP(ETA(1))") %>%
+    expect_contains("VP = THETA(2) * EXP(ETA(2))") %>%
+    expect_contains("Q = THETA(3) * EXP(ETA(3))") %>%
+    expect_contains("CL = THETA(4) * EXP(ETA(4))") %>%
+    expect_contains("V1 = VC") %>%
+    expect_contains("V2 = VP") %>%
+    expect_contains("CONC = A(1)/VC") %>%
+    expect_contains("Y = CONC + EPS(1)")
+})
+
 
 test_that("1cmp linear 1st order absorption, advan", {
   m <- pk_model() +
