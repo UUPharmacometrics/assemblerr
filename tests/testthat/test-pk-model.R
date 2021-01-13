496e70f1cef6ae9d700b72b2dd65ef396efe9535
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2021-01-13 13:09:29 +0100

    add michaelis menten test

--- tests/testthat/test-pk-model.R
+++ tests/testthat/test-pk-model.R
@@ -98,6 +98,24 @@ test_that("2cmp linear", {
     expect_contains("Y = CONC + EPS(1)")
 })
 
+test_that("1cmp mm", {
+  m <- pk_model() +
+    pk_distribution_1cmp() +
+    pk_elimination_mm() +
+    obs_additive(conc~C["central"])
+
+  render(m,
+         options = assemblerr_options(ode.use_special_advans = TRUE,
+                                      ode.use_general_linear_advans = TRUE)
+  ) %>%
+    expect_contains("VC = THETA(1) * EXP(ETA(1))") %>%
+    expect_contains("CLMM = THETA(2) * EXP(ETA(2))") %>%
+    expect_contains("KM = THETA(3) * EXP(ETA(3))") %>%
+    expect_contains("DADT(1) = -(CLMM * KM/(KM + A(1)/VC))") %>%
+    expect_contains("CONC = A(1)/VC") %>%
+    expect_contains("Y = CONC + EPS(1)")
+})
+
 
 test_that("1cmp linear 1st order absorption, advan", {
   m <- pk_model() +
