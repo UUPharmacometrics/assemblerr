2bb1fdd3531a6392e43e79ff8d0bda867ca1308b
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2021-01-15 12:48:00 +0100

    export dataset building blocks

--- R/input_variable.R
+++ R/input_variable.R
@@ -15,6 +15,7 @@ InputVariableFacet <- setClass(
                         label = "input variables")
 )
 
+#' @export
 input_variable <- function(name){
   if (!is.character(name)) stop("'name' needs to be a character vector")
   InputVariable(name = name)
@@ -22,8 +23,9 @@ input_variable <- function(name){
 
 #' @importFrom utils read.csv
 #' @importFrom utils read.table
+#' @export
 dataset <- function(path){
-  if (grepl(".csv$",path)){
+  if (grepl(".csv$",path)) {
     tab <- read.csv(path, header = TRUE, nrows = 1)
   }else{
     tab <- read.table(path, header = T, nrows = 1)
