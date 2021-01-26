5fdbfdd9ec961e18f1a14b8245166ccb61ab3bd7
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2021-01-26 15:50:06 +0100

    add parameter model documentation

--- R/parameter.R
+++ R/parameter.R
@@ -39,8 +39,62 @@ assert_valid_parameter_name <- function(name) {
 
 PrmLogNormal <- setClass("PrmLogNormal",
                          contains = "Parameter")
-
+#' Parameter model
+#'
+#' These building blocks define a parameter model, declaring a parameter and specifying how it varies between subjects.
+#'
+#' The parameter building blocks require a name for the parameter by which it can be referenced in a different building
+#' block. The distribution of the parameter in the population is chosen by selecting among the following predefined
+#'  functions:
+#'   * `prm_no_var`: A parameter without variability, i.e., without random effects.
+#'   * `prm_normal`: A parameter following the normal distribution.
+#'   * `prm_log_normal`: A parameter following the log-normal distribution.
+#'   * `prm_logit_normal`: A parameter following the logit-normal distribution (the parameter is normally distributed on the logit scale).
+#'
+#' Adding a parameter with an already existing name will replace the definition of the parameter. For example, the parameter "base"
+#' will have a log-normal distribution in the following snippet:
+#' ```
+#' m <- model() +
+#'   prm_normal("base") +
+#'   prm_log_normal("base")
+#' ```
+#'
+#' ## MU-referencing
+#'
+#' `assemblerr` can include mu-referencing statements for parameter distributions that support it. The functionality can be
+#' activated by setting the option `prm.use_mu_referencing` to `TRUE` as shown in the following snippet:
+#' ```
+#' m <- model() +
+#'    prm_normal("base") +
+#'    prm_log_normal("slp") +
+#'    obs_additive(response~base+slp*time)
+#'
+#' render(
+#'   model = m,
+#'   options = assemblerr_options(prm.use_mu_referencing = TRUE)
+#' )
+#' ```
+#'
+#'
+#' @param name A name for the parameter
+#'
+#' @examples
+#' # a log-normal parameter that is directly observed
+#' m <- model() +
+#'   prm_log_normal("wt") +
+#'   obs_additive(~wt)
+#'
+#' # EMAX dose-response model with emax and ed50 parameters
+#' m2 <- model() +
+#'   input_variable("dose") +
+#'   prm_no_var("emax") +
+#'   prm_no_var("ed50") +
+#'   obs_proportional(effect~emax*dose/(ed50+dose))
 #' @export
+#' @name parameter-model
+#' @rdname parameter-model
+#' @order 3
+#' @md
 prm_log_normal <- function(name) {
   assert_valid_parameter_name(name)
   PrmLogNormal(name = name)
@@ -53,6 +107,8 @@ PrmNormal <- setClass("PrmNormal",
                       contains = "Parameter")
 
 #' @export
+#' @rdname parameter-model
+#' @order 2
 prm_normal <- function(name) {
   assert_valid_parameter_name(name)
   PrmNormal(name = name)
@@ -65,6 +121,8 @@ PrmLogitNormal <- setClass("PrmLogitNormal",
                       contains = "Parameter")
 
 #' @export
+#' @rdname parameter-model
+#' @order 4
 prm_logit_normal <- function(name) {
   assert_valid_parameter_name(name)
   PrmLogitNormal(name = name)
@@ -76,6 +134,8 @@ PrmNoVar <- setClass("PrmNoVar",
                          contains = "Parameter")
 
 #' @export
+#' @rdname parameter-model
+#' @order 1
 prm_no_var <- function(name) {
   assert_valid_parameter_name(name)
   PrmNoVar(name = name)
