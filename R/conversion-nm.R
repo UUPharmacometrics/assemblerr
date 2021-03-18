5cc4a2530181fd8430e5acaf9c6c854c166d172f
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2021-03-18 09:02:19 +0100

    remove NULL check for default records

--- R/conversion-nm.R
+++ R/conversion-nm.R
@@ -12,8 +12,8 @@ setMethod(
       convert(source, source@facets[["ObservationFacet"]], options) %>%
       convert(source, source@facets[["InputVariableFacet"]], options) %>%
       convert(source, source@facets[["MetadataFacet"]], options)
-    if (!is.null(options[["default_record.covariance_step"]])) target <- target + options[["default_record.covariance_step"]]
-    if (!is.null(options[["default_record.estimation_step"]])) target <- target + options[["default_record.estimation_step"]]
+    target <- target + options[["default_record.covariance_step"]]
+    target <- target + options[["default_record.estimation_step"]]
     if (vec_is_empty(source@facets[["InputVariableFacet"]]@entries)) {
       target  <-    target +
         nm_input("id", "id") +
