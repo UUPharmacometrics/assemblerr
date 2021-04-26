eb474a6d49feb4904bf1f8cb399f987dace97031
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2021-04-26 11:25:05 +0200

    change expressions to character vectors

--- R/conversion-parameter-nm.R
+++ R/conversion-parameter-nm.R
@@ -17,17 +17,16 @@ setMethod(
     eta_index <- index_of(target@facets$NmOmegaParameterFacet, component@name)
 
     if (options$prm.use_mu_referencing) {
-      mu_name <- sym(paste0("mu_",eta_index))
       target <- target + nm_pk(
         statement(
-          bquote(.(mu_name) <- log(theta[.(theta_index)]))
+          "{paste0('mu_',eta_index)} <- log(theta[{theta_index}])"
         )
       )
     }
     target +
       nm_pk(
         statement(
-          bquote(.(sym(component@name)) <- theta[.(theta_index)]*exp(eta[.(eta_index)]))
+          "{component@name} <- theta[{theta_index}]*exp(eta[{eta_index}])"
         )
       )
   }
@@ -53,14 +52,14 @@ setMethod(
       mu_name <- sym(paste0("mu_",eta_index))
       target <- target + nm_pk(
         statement(
-          bquote(.(mu_name) <- theta[.(theta_index)])
+          "{paste0('mu_',eta_index)} <- theta[{theta_index}]"
         )
       )
     }
     target +
       nm_pk(
         statement(
-          bquote(.(sym(component@name)) <- theta[.(theta_index)] + eta[.(eta_index)])
+          "{component@name} <- theta[{theta_index}] + eta[{eta_index}]"
         )
       )
   }
@@ -82,20 +81,19 @@ setMethod(
     theta_index <- index_of(target@facets$NmThetaParameterFacet, component@name)
     eta_index <- index_of(target@facets$NmOmegaParameterFacet, component@name)
     if (options$prm.use_mu_referencing) {
-      mu_name <- sym(paste0("mu_",eta_index))
       target <- target + nm_pk(
         statement(
-          bquote(.(mu_name) <- log(theta[(theta_index)])/(1 - log(theta[.(theta_index)])))
+          "{paste0('mu_',eta_index)} <- log(theta[{theta_index}])/(1 - log(theta[{theta_index}]))"
         )
       )
     }
-    sym_prm_logit <- sym(paste0("logit_", component@name))
-    sym_prm <- sym(component@name)
+    prm_logit <- paste0("logit_", component@name)
+    prm <- component@name
     target +
       nm_pk(
         statement(
-          bquote(.(sym_prm_logit) <- log(theta[.(theta_index)]/(1 - theta[.(theta_index)])) + eta[.(eta_index)]),
-          bquote(.(sym_prm) <- exp(.(sym_prm_logit))/(1 + exp(.(sym_prm_logit))))
+          "{prm_logit} <- log(theta[{theta_index}]/(1 - theta[{theta_index}])) + eta[{eta_index}]",
+          "{prm} <- exp({prm_logit})/(1 + exp({prm_logit}))"
         )
       )
   }
@@ -118,7 +116,7 @@ setMethod(
     target +
       nm_pk(
         statement(
-          bquote(.(sym(component@name)) <- theta[.(theta_index)])
+          "{component@name} <- theta[{theta_index}]"
         )
       )
   }
