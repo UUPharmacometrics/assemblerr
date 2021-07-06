786f8c891c1f52e8d9fea30f13ce0832ab94bd5f
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2021-07-06 08:46:51 +0200

    document options function (fixes #22)

--- R/model-options.R
+++ R/model-options.R
@@ -1,3 +1,20 @@
+
+#' Options
+#'
+#' This function creates a list of options for the use with the render function.
+#'
+#' The function helps to create properly formatted list that can serve as input to the `options=` argument
+#' of the `render()` function.
+#'
+#' @param prm.use_mu_referencing Use mu-referencing?
+#' @param ode.use_special_advans Use analytic solution ADVANs?
+#' @param ode.use_general_linear_advans Use ADVANs for linear ODEs?
+#' @param ode.general_nonlinear_advan ADVAN to be used for non-linear ODEs
+#' @param ode.general_linear_advan ADVAN to be used for linear ODEs
+#' @param ode.preferred_trans_routines Order of TRANS routines to be tried
+#' @param issues.missing_variables How to handle missing variables
+#'
+#' @md
 #' @export
 assemblerr_options <- function(prm.use_mu_referencing = FALSE,
                        ode.use_special_advans = TRUE,
@@ -5,8 +22,6 @@ assemblerr_options <- function(prm.use_mu_referencing = FALSE,
                        ode.general_nonlinear_advan = "advan13",
                        ode.general_linear_advan = "advan5",
                        ode.preferred_trans_routines = c("trans2", "trans4"),
-                       default_record.covariance_step = nm_covariance(),
-                       default_record.estimation_step = nm_estimation(),
                        issues.missing_variables = c("fix-warn", "fix", "ignore", "fail")) {
   # TODO: add 'warn' option for issues.missing_variables
   return(
